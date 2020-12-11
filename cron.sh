615860790e0c927bee96bf817c82b6da56acc067
idk <hankhill19580@gmail.com>
2020-12-11 14:53:40 -0500

    fix crontab generator

--- cron.sh
+++ cron.sh
@@ -4,5 +4,5 @@ timeza=$(date +%z | tr -d '\-0')
 
 bundle_wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo 0 $timeza 10 0 0 $bundle_wd/seed.sh | tee cronjob
+echo 0 $timeza 10 \* \* $bundle_wd/seed.sh | tee cronjob
 
--- fetch.sh
+++ fetch.sh
@@ -5,6 +5,8 @@ if [ -z $bundle_wd ]; then
   bundle_wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 fi
 
+echo $bundle_wd
+
 if [ ! -d $bundle_wd/i2p.i2p ]; then
   git clone https://i2pgit.org/i2p-hackers/i2p.i2p.git $bundle_wd/i2p.i2p
 fi
